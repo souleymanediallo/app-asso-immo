7dc9891f9fd982fc1acc0c3c16ef780c2ac8816a
souleymane <souleymanedialloo@yahoo.fr>
2021-07-14 10:48:56 +0200

    add register

---
+++ accounts/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import CustomUser
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ["email", "username", "password1", "password2"]
--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('account/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
